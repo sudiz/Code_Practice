c25b68d3523f94093b8e19b8a42ac12376657f9a
sudiz <70258488+sudiz@users.noreply.github.com>
2024-10-11 22:31:28 +1100

    Create 534. Game Play Analysis III(SQL).sql

---
+++ LeetCode/534. Game Play Analysis III(SQL).sql
@@ -0,0 +1,7 @@
+select a1.player_id,a1.event_date,sum(a2.games_played) as games_played_so_far
+from Activity a1
+join Activity a2
+on a1.player_id=a2.player_id
+and a1.event_date>=a2.event_date
+group by a1.player_id, a1.event_date
+order by a1.player_id, a1.event_date
