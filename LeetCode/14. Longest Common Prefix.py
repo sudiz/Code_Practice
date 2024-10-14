19cb114cf147dc0ba51b645946e8eb3ed14e0064
sudiz <70258488+sudiz@users.noreply.github.com>
2024-10-14 11:57:30 +1100

    Update 14. Longest Common Prefix.py

--- LeetCode/14. Longest Common Prefix.py
+++ LeetCode/14. Longest Common Prefix.py
@@ -11,3 +11,14 @@ class Solution:
             else:
                 break;
         return res
+# another version
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res=''
+        for i in range(0,len(strs[0])):
+            for j in range(1,len(strs)):
+                if i>=len(strs[j]) or strs[j][i]!=strs[0][i]:
+                    return res
+            res+=strs[0][i]
+        return res
